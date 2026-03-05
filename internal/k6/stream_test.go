62ab9e34dea004f9f013f7a5ccabca14dca63288
Gabriel Freschi <63794779+gfreschi@users.noreply.github.com>
2026-03-05 09:49:32 -0300

    refactor(k6): align package name with directory name
    Signed-off-by: Gabriel Freschi <63794779+gfreschi@users.noreply.github.com>

--- internal/k6/stream_test.go
+++ internal/k6/stream_test.go
@@ -1,15 +1,15 @@
-package k6runner_test
+package k6_test
 
 import (
 	"testing"
 
-	k6runner "github.com/gfreschi/k6delta/internal/k6"
+	"github.com/gfreschi/k6delta/internal/k6"
 )
 
 func TestParseK6JSONLine_metric(t *testing.T) {
 	line := `{"type":"Point","data":{"time":"2026-03-04T12:00:00Z","value":234.5},"metric":"http_reqs","tags":{}}`
 
-	point, err := k6runner.ParseJSONLine(line)
+	point, err := k6.ParseJSONLine(line)
 	if err != nil {
 		t.Fatalf("ParseJSONLine() error = %v", err)
 	}
@@ -24,7 +24,7 @@ func TestParseK6JSONLine_metric(t *testing.T) {
 func TestParseK6JSONLine_ignoresNonPoint(t *testing.T) {
 	line := `{"type":"Metric","data":{"name":"http_reqs","type":"counter"}}`
 
-	point, err := k6runner.ParseJSONLine(line)
+	point, err := k6.ParseJSONLine(line)
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
